744d731b99f2163be23f2d54ed23fbb7ace2e85e
David Shin <shindavid951@gmail.com>
2024-04-25 00:07:11 -0400

    Altered Game.play() to take two players. TODO: Write the actual game loop.

--- game.py
+++ game.py
@@ -1,11 +1,12 @@
 import discord
 import asyncio
 from discord.ext import commands
+from deck import Deck
 
 class Game(object):
     def __init__(self, bot=None, server=None):
         self.bot = bot
         self.server = server
 
-    async def play(self, ctx):
+    async def play(self, ctx, playerOne, playerTwo):
         pass
