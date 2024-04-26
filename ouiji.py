786e081ccc56c222a6412c7661ad59d7692064f4
David Shin <shindavid951@gmail.com>
2024-04-25 20:56:22 -0400

    Added consensual dueling

--- ouiji.py
+++ ouiji.py
@@ -6,11 +6,11 @@ import sys
 import PIL.Image
 
 class Ouiji(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def play(self, ctx, member: discord.Member = None):
+    async def play(self, ctx: commands.Context, member: discord.Member = None):
         """Ping the user you want to duel!"""
         channel_id = ctx.channel.id
         # Save for later
@@ -36,6 +36,16 @@ class Ouiji(commands.Cog):
             await ctx.send("You can't duel against yourself!")
         elif member == self.bot.user:
             await ctx.send(f"I'd like to duel with you, but I can't. {str(discord.utils.get(self.bot.emojis, name='defeat'))}")
+        elif member.bot:
+            await ctx.send(f"Beep boop. Shouldn't you play against a real person?")
         else:
-            await ctx.send("It's time to duel!")
-            await Game(self.bot, ctx.message.server).play(ctx, ctx.author, member)
+            def check(m):
+                return m.channel.id == channel_id and m.author == member
+            await ctx.send(f"{member.display_name}, would you like to duel with {ctx.author.display_name}? Say 'yes' to accept. Any other response will count as a decline.")
+            response = await self.bot.wait_for('message', check=check)
+            if response.content.lower() == "yes":
+                await ctx.send("It's time to duel!")
+                await Game(self.bot, ctx.message.server).play(ctx, ctx.author, member)
+            else:
+                await ctx.send("Duel declined.")
+                return
