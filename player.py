4f2d40702624edd205ec251ec2b50b7e2c1f72fc
David Shin <shindavid951@gmail.com>
2024-04-26 15:45:23 -0400

    Enforced list types

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
 import discord
+from typing import List
+from card import Card
 class Player(object):
     def __init__(self, member: discord.Member):
         self.member = member
-        self.hand = []
-        self.field = []
+        self.hand: List[Card] = []
+        self.field: List[Card] = []
         self.spirits = 0
