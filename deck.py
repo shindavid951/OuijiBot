18ee4c43cdc2a956c474994a854041c564333af9
David Shin <shindavid951@gmail.com>
2024-04-26 00:47:41 -0400

    Forgot to add VengefulEmpress

--- deck.py
+++ deck.py
@@ -6,6 +6,7 @@ class Deck():
             FoolishIndecisive,
             PoisonousMagician,
             LoneHighPriestess,
+            VengefulEmpress,
             CrushingEmperor,
             WiseHierophant,
             LoversBeneath,
