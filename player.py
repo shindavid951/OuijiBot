42c140a53df5636e7c0c3c7341e1027a35a05d87
David Shin <shindavid951@gmail.com>
2024-04-26 21:00:27 -0400

    Changed list property to be a list with three elements to represent the three lanes

--- player.py
+++ player.py
@@ -5,5 +5,5 @@ class Player(object):
     def __init__(self, member: discord.Member):
         self.member = member
         self.hand: List[Card] = []
-        self.field: List[UnitCard] = []
+        self.field: List[UnitCard] = [None, None, None]
         self.spirits = 0
