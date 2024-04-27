26adc09a9db9c65505e549bbf8c8a98060c67a15
David Shin <shindavid951@gmail.com>
2024-04-27 18:29:41 -0400

    Implemented slash commands

--- game.py
+++ game.py
@@ -1,12 +1,17 @@
 import discord
 import PIL.Image
+import json
 from discord.ext import commands
+from discord import app_commands
 from deck import Deck
 from player import Player
 from random import randint
 
-class Game(object):
-    def __init__(self, bot: commands.Bot, server: discord.Guild, player_one: Player, player_two: Player):
+with open("config.json", "r") as cfg:
+    data = json.load(cfg)
+
+class Game(commands.Cog):
+    def __init__(self, bot: commands.Bot, server: discord.Guild=None, player_one: Player=None, player_two: Player=None):
         self.bot = bot
         self.server = server
         self.turn = 0
@@ -45,14 +50,14 @@ class Game(object):
             #   The Lone High Priestess
             #   or The Vengeful Empress
             await ctx.send(f"{self.current_player.member.display_name}, choose your first Card (Say 1, 2, or 3):")
-            await self.show_cards(ctx, [deck.deck[1], deck.deck[2], deck.deck[3]], True)
+            await self.show_cards([deck.deck[1](), deck.deck[2](), deck.deck[3]()], ctx, None, True)
             def check(m):
                 return m.channel.id == ctx.channel.id and m.author == self.current_player.member
             choice = await self.bot.wait_for('message', check=check)
             while int(choice.content) not in [1, 2, 3]:
                 await ctx.send("Invalid choice. Please choose again.")
                 choice = await self.bot.wait_for('message', check=check)
-            self.current_player.hand.append(deck.deck[choice]())
+            self.current_player.hand.append(deck.deck[int(choice.content)]())
             # If it's player one's turn, they draw 3 more cards in addition to the ones the Fool and the card they chose.
             if self.turn % 2 == 0:
                 for _ in range(3):
@@ -73,7 +78,6 @@ class Game(object):
             # Adds the Foolish Indecisive to the current player's hand and ends the draw phase.
             if fool_draw.content.lower() == "yes":
                 self.current_player.hand.append(deck.deck[0]())
-                self.show_cards(ctx, self.current_player.hand, False, True)
                 return
             self.current_player.hand.append(deck.deck[randint(1, len(deck.deck))]())
         # The player did not spend spirits and did not forgo their draw, so the draw phase now ends
@@ -87,8 +91,8 @@ class Game(object):
     async def combat_phase(self):
         self.current_phase = "Combat"
     
-    async def show_cards(self, ctx: commands.Context, cards, first_turn=False, hand=False, field=False):
-        images = [PIL.Image.open(x().image) for x in cards]
+    async def show_cards(self, cards, ctx: commands.Context = None, interaction: discord.Interaction = None, first_turn=False, hand=False, field=False):
+        images = [PIL.Image.open(x.image) for x in cards]
         widths, heights = zip(*(i.size for i in images))
         total_width = sum(widths)
         max_height = max(heights)
@@ -107,7 +111,7 @@ class Game(object):
                 if hand:
                     cards_image.save("./hand_images/player_one_hand.png")
                     hand_fp = open("./hand_images/player_one_hand.png", "rb")
-                    await ctx.send(file=discord.File(hand_fp))
+                    await interaction.response.send_message(file=discord.File(hand_fp), ephemeral=True)
                     hand_fp.close()
                 elif field:
                     cards_image.save("./hand_images/player_one_field.png")
@@ -118,7 +122,7 @@ class Game(object):
                 if hand:
                     cards_image.save("./hand_images/player_two_hand.png")
                     hand_fp = open("./hand_images/player_two_hand.png", "rb")
-                    await ctx.send(file=discord.File(hand_fp))
+                    await interaction.response.send_message(file=discord.File(hand_fp), ephemeral=True)
                     hand_fp.close()
                 elif field:
                     cards_image.save("./hand_images/player_two_field.png")
@@ -127,38 +131,48 @@ class Game(object):
                     field_fp.close()
 
     # The current player can choose to spend 1 or 2 spirits during their draw or main phase in order to draw an additional 1 or 2 cards.
-    @commands.command()
-    async def spirits(self, ctx: commands.Context):
+    @app_commands.command(name="spend_spirits", description="Spend 1 or 2 spirits in exchange for drawing addtional cards.")
+    @app_commands.guilds(data["guild_id"])
+    @app_commands.describe(spirits = "How many spirits do you want to spend? (1 or 2)")
+    async def spirits(self, interaction: discord.Interaction, spirits: int):
+        if interaction.user != self.current_player.member:
+            await interaction.response.defer()
+            await interaction.followup.send("It's not your turn yet!")
+            return
         if self.current_player.spirits > 0 and self.current_phase not in ["Draw", "Main"]:
             deck = Deck()
-            ctx.send("Would you like to spend spirits to draw additional cards? (yes/no)")
-            def check(m):
-                if m.author != self.current_player.member:
-                    ctx.send("It's not your turn yet!")
-                    return
-                return m.channel.id == ctx.channel.id and m.author == self.current_player.member
-            additional_draw = await self.bot.wait_for('message', check=check)
-            while additional_draw.content.lower() not in ["yes", "no"]:
-                await ctx.send("Sorry, I didn't get that. Would you like to spend spirits to draw additional cards? (yes/no)")
-            if additional_draw.content.lower() == "yes":
-                ctx.send("How many spirits would you like to spend? (1 or 2)")
-                spirits_spent = await self.bot.wait_for('message', check=check)
-                while spirits_spent.content.lower() not in ["1", "2", "one", "two"]:
-                    await ctx.send("Sorry, I didn't get that. How many spirits would you like to spend? (1 or 2)")
-                # Subtracts the appropriate amount of spirits from the player's spirit count, 
-                # draws the appropriate number of cards, and ends the draw phase.
-                if spirits_spent.content.lower() in ["1", "one"]:
-                    self.current_player.spirits -= 1
+            # Subtracts the appropriate amount of spirits from the player's spirit count, 
+            # draws the appropriate number of cards, and ends the draw phase.
+            if spirits == 1:
+                self.current_player.spirits -= 1
+                self.current_player.hand.append(deck.deck[randint(1, len(deck.deck))]())
+                return
+            elif spirits == 2:
+                self.current_player.spirits -= 2
+                for _ in range(2):
                     self.current_player.hand.append(deck.deck[randint(1, len(deck.deck))]())
                     return
-                elif spirits_spent.content.lower() in ["2", "two"]:
-                    self.current_player.spirits -= 2
-                    for _ in range(2):
-                        self.current_player.hand.append(deck.deck[randint(1, len(deck.deck))]())
-                        return
+            else:
+                await interaction.response.defer()
+                await interaction.followup.send("Invalid number of spirits entered.")
+                return
         else:
             if self.current_phase not in ["Draw", "Main"]:
-                ctx.send("You can't spend spirits right now.")
+                await interaction.response.defer()
+                await interaction.followup.send("You can't spend spirits right now.")
                 return
-            ctx.send("You don't have enough spirits to spend right now.")
+            await interaction.response.defer()
+            await interaction.followup.send("You don't have enough spirits to spend right now.")
+            return
+        
+    @app_commands.command(name="show_hand", description="Shows you your current hand")
+    @app_commands.guilds(data["guild_id"])
+    async def show_hand(self, interaction: discord.Interaction):
+        if interaction.user == self.player_one.member:
+            await self.show_cards(self.player_one.hand, interaction=interaction, hand=True)
+        elif interaction.user == self.player_two.member:
+            await self.show_cards(self.player_two.hand, interaction=interaction, hand=True)
+        else:
+            await interaction.response.defer()
+            await interaction.followup.send("You aren't playing in this game!")
             return
--- ouiji.py
+++ ouiji.py
@@ -1,39 +1,46 @@
 from discord.ext import commands
+from discord import app_commands
 from game import Game
 from player import Player
 import discord
-import asyncio
 from random import randint
+import json
+
+with open("config.json", "r") as cfg:
+    data = json.load(cfg)
 
 class Ouiji(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.command()
-    async def play(self, ctx: commands.Context, member: discord.Member = None):
-        """Ping the user you want to duel!"""
-        channel_id = ctx.channel.id
-        # Checks to see if the pinged user is valid
-        if member is None:
-            await ctx.send("Usage: !play [ping person you want to duel]")
+    @app_commands.command(name="play", description="Play a game of Ou-i-ji against someone!")
+    @app_commands.guilds(data["guild_id"])
+    @app_commands.describe(opponent = "Who would you like to play against?")
+    async def play(self, interaction: discord.Interaction, opponent: discord.Member):
+        ctx : commands.Context = await self.bot.get_context(interaction)
+        channel_id = interaction.channel_id
+        # Checks to see if the given user is valid
         # elif member == ctx.author:
             # await ctx.send("You can't duel against yourself!")
-        elif member == self.bot.user:
-            await ctx.send(f"I'd like to duel with you, but I can't. {str(discord.utils.get(self.bot.emojis, name='defeat'))}")
-        elif member.bot:
-            await ctx.send(f"Beep boop. Shouldn't you play against a real person?")
+        if opponent == self.bot.user:
+            await interaction.response.send_message(f"I'd like to duel with you, but I can't. {str(discord.utils.get(self.bot.emojis, name='defeat'))}")
+        elif opponent.bot:
+            await interaction.response.send_message(f"Beep boop. Shouldn't you play against a real person?")
         else:
             # Checks to see if the opponent responded to the bot in the right channel
             def check(m):
-                return m.channel.id == channel_id and m.author == member
-            await ctx.send(f"{member.display_name}, would you like to duel with {ctx.author.display_name}? Say 'yes' to accept. Any other response will count as a decline.")
+                return m.channel.id == channel_id and m.author == opponent
+            await interaction.response.defer()
+            await interaction.followup.send(f"{opponent.display_name}, would you like to duel with {interaction.user.display_name}? Say 'yes' to accept. Any other response will count as a decline.")
             response = await self.bot.wait_for('message', check=check)
             if response.content.lower() == "yes":
-                await ctx.send("It's time to duel!")
-                players = [Player(ctx.author), Player(member)]
+                await interaction.followup.send("It's time to duel!")
+                players = [Player(interaction.user), Player(opponent)]
                 player_one = players[randint(0, 1)]
                 player_two = players[1] if player_one == players[0] else players[0]
-                await Game(self.bot, ctx.message.guild, player_one, player_two).play(ctx)
+                game = Game(self.bot, interaction.guild, player_one, player_two)
+                await self.bot.add_cog(game)
+                await game.play(ctx)
             else:
-                await ctx.send("Duel declined.")
+                await interaction.followup.send("Duel declined.")
                 return
